d6ce22898aa7dd3acee4840d91af0c3a0fa6ea12
IcedCodes <435642922@qq.com>
2018-02-22 23:15:02 +0800

    注释
    为POJ1742 Coins添加合适注释

--- POJ/1742 Coins.cpp
+++ POJ/1742 Coins.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+/*
+n为币值总数，m为最大值,a[i]币值，c[i]数目，
+dp[i][j]表示用到币值a[i]的硬币时凑成币值j时币值a[i]的硬币剩余硬币数量 
+*/
 int main()
 {
 	int n, m, i, j, count;
@@ -17,15 +21,15 @@ int main()
 		{
 			cin >> c[i];
 		}
-		memset(dp, -1, sizeof(dp));
+		memset(dp, -1, sizeof(dp));		//全部初始化为-1
 		dp[0] = 0;
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j <= m; j++)
 			{
-				if (dp[j] >= 0)dp[j] = c[i];
-				else if (j < a[i])dp[j] = -1;
-				else dp[j] = dp[j - a[i]] - 1;
+				if (dp[j] >= 0)dp[j] = c[i];	//dp[j]即dp[i - 1][j],先前能凑得就不用消耗 
+				else if (j < a[i])dp[j] = -1;	//先前币值的硬币未能凑得且j小于当前a[i]币值则设-1表无法凑得 
+				else dp[j] = dp[j - a[i]] - 1;	//dp[j-a[i]]再用一个a[i]凑得币值j 
 			}
 		}
 		count = 0;
